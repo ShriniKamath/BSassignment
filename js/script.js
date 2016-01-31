9b9df381577178257abed09aafe54daac41517c2
unknown <shrinivaskamath143@gmail.com>
2016-01-31 17:44:34 +0530

    Bootstrap assignment

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/*
+
+My Custom JS
+============
+
+Author:  Shrinivas Kamath
+Updated: Jan 2016
+
+*/
+//Used on the both the page for the login button
+$(function(){
+  $('#joinMe').click(function(e){
+    e.preventDefault();
+    $('#successAlert').slideDown();
+  });
+  $('#successAlertx').click(function(e){
+    e.preventDefault();
+    $('#successAlert').slideUp();
+  });
+//Used on the page services for the Book_Now button
+  $('[rel="tooltip"]').tooltip();
+
+  $('#facebookid').click(function() {
+$('#successAlert').hide();
+
+
+})
+});
